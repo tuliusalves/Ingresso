a044fa1dcd8fa43ff2c6682a3e40d7973de288e3
Túlius <tulius_sr@outlook.com>
2023-12-04 15:21:41 -0300

    Adicionado menu contendo a lógica do programa, classe Ingresso agora é abstract

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,56 @@
+import entities.CamaroteInferior;
+import entities.CamaroteSuperior;
+import entities.Ingresso;
+import entities.Normal;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        Ingresso ingresso;
 
-        System.out.println("Hello world!");
+        int opcao=0;
+
+        do {
+            System.out.println("\nVenda de Ingressos, qual ingresso deseja comprar");
+            System.out.println("1(Ingresso normal)\t2(VIP - Camarote inferior)\t " +
+                    "3(VIP - Camarote superior)\t 0(Fechar o programa)");
+            opcao = scan.nextInt();
+            // Faça o objeto ingresso se comportar como os demais
+            switch (opcao){
+                case 1:
+                    System.out.println("Deseja confirmar a compra?");
+                    String resposta= scan.next();
+                    if(resposta.equalsIgnoreCase("Sim")) {
+                        System.out.println("Compra efetuada!");
+                        ingresso = new Normal();
+                        ingresso.imprimirNormal();
+                        ingresso.imprimirValor();
+                    }
+                    break;
+                case 2:
+                    System.out.println("Deseja confirmar a compra?");
+                    resposta= scan.next();
+                    if(resposta.equalsIgnoreCase("Sim")) {
+                        System.out.println("Compra efetuada!");
+                        ingresso= new CamaroteInferior();
+                        ingresso.imprimirValorAdicional();
+                    }
+                    break;
+                case 3:
+                    System.out.println("Deseja confirmar a compra?");
+                    resposta= scan.next();
+                    if(resposta.equalsIgnoreCase("Sim")) {
+                        System.out.println("Compra efetuada!");
+                        ingresso = new CamaroteSuperior();
+                        ingresso.imprimirRelatorio();
+                    }
+                     break;
+                default:
+                    opcao=0;
+                    break;
+            }
+        }while (opcao!=0);
     }
 }
--- src/entities/Ingresso.java
+++ src/entities/Ingresso.java
@@ -1,12 +1,12 @@
 package entities;
 
-public class Ingresso {
+public abstract class Ingresso {
     private double valor;
 
     public Ingresso(){}
 
     public double getValor() {
-        return valor;
+        return 100;
     }
 
     public void setValor(double valor) {
@@ -15,5 +15,14 @@ public class Ingresso {
 
     public void imprimirValor(){
         System.out.println("O valor do ingresso R$:"+getValor());
+    }
+
+    public void imprimirValorAdicional() {
+    }
+
+    public void imprimirRelatorio() {
+    }
+
+    public void imprimirNormal() {
     }
 }
